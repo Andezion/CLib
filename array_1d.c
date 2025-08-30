09f2e0786b7096109897a352a0a39372adad3c2f
Andezion <245122@edu.p.lodz.pl>
2025-08-30 11:13:44 +0200

    added float array

--- array_1d.c
+++ array_1d.c
@@ -1,6 +1,6 @@
 #include "array_1d.h"
 
-void destroy_array_1d(int64_t **ptr, const size_t size)
+void destroy_array_i_1d(int64_t **ptr, const size_t size)
 {
     if (ptr == NULL || size == 0)
     {
@@ -8,10 +8,20 @@ void destroy_array_1d(int64_t **ptr, const size_t size)
     }
 
     free(*ptr);
-    ptr = NULL;
+    *ptr = NULL;
+}
+void destroy_array_f_1d(float64_t **ptr, const size_t size)
+{
+    if (ptr == NULL || size == 0)
+    {
+        return;
+    }
+
+    free(*ptr);
+    *ptr = NULL;
 }
 
-int create_array_1d(int64_t **ptr, const size_t size)
+int create_array_i_1d(int64_t **ptr, const size_t size)
 {
     if (ptr == NULL || size == 0)
     {
@@ -28,29 +38,68 @@ int create_array_1d(int64_t **ptr, const size_t size)
     return 0;
 }
 
-int create_array_1d_init(int64_t **ptr, const size_t size, const int64_t value)
+int create_array_i_1d_init(int64_t **ptr, const size_t size, const int64_t value)
 {
     if (ptr == NULL || size == 0)
     {
         return 1;
     }
 
-    int64_t *array = malloc(sizeof(int64_t) * size);
-    if (array == NULL)
+    const int error = create_array_i_1d(ptr, size);
+    if (error != 0)
     {
-        return 2;
+        return error;
     }
 
+    int64_t *array = *ptr;
     for (size_t i = 0; i < size; i++)
     {
         *(array + i) = value;
     }
+
+    return 0;
+}
+
+int create_array_f_1d(float64_t **ptr, const size_t size)
+{
+    if (ptr == NULL || size == 0)
+    {
+        return 1;
+    }
+
+    float64_t *array = malloc(sizeof(float64_t) * size);
+    if (array == NULL)
+    {
+        return 2;
+    }
     *ptr = array;
 
     return 0;
 }
 
-void display_array_1d(const int64_t *ptr, const size_t size)
+int create_array_f_1d_init(float64_t **ptr, const size_t size, const float64_t value)
+{
+    if (ptr == NULL || size == 0)
+    {
+        return 1;
+    }
+
+    const int error = create_array_f_1d(ptr, size);
+    if (error != 0)
+    {
+        return error;
+    }
+
+    float64_t *array = *ptr;
+    for (size_t i = 0; i < size; i++)
+    {
+        *(array + i) = value;
+    }
+
+    return 0;
+}
+
+void display_array_i_1d(const int64_t *ptr, const size_t size)
 {
     if (ptr == NULL || size == 0)
     {
@@ -60,5 +109,18 @@ void display_array_1d(const int64_t *ptr, const size_t size)
     {
         printf("%" PRId64 " ", *(ptr + i));
     }
+    printf("\n");
+}
+void display_array_f_1d(const float64_t *ptr, const size_t size)
+{
+    if (ptr == NULL || size == 0)
+    {
+        return;
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%f ", *(ptr + i));
+    }
+    printf("\n");
 }
 
--- array_1d.h
+++ array_1d.h
@@ -5,11 +5,16 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-void destroy_array_1d(int64_t **ptr, size_t size);
+typedef double float64_t;
 
-int create_array_1d(int64_t **ptr, size_t size);
-int create_array_1d_init(int64_t **ptr, size_t size, int64_t value);
+void destroy_array_i_1d(int64_t **ptr, size_t size);
+void destroy_array_f_1d(float64_t **ptr, size_t size);
 
+int create_array_i_1d(int64_t **ptr, size_t size);
+int create_array_f_1d_init(int64_t **ptr, size_t size, int64_t value);
 
+int create_array_f_1d(float64_t **ptr, size_t size);
+int create_array_f_1d_init(float64_t **ptr, size_t size, float64_t value);
 
-void display_array_1d(const int64_t *ptr, size_t size);
+void display_array_i_1d(const int64_t *ptr, size_t size);
+void display_array_f_1d(const float64_t *ptr, size_t size);
