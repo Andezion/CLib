5b8d81016d980b626b9779d135eed547e5d528e8
Andezion <245122@edu.p.lodz.pl>
2025-09-23 22:36:52 +0200

    added correct add func

--- array_2d.h
+++ array_2d.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdlib.h>
 #include <inttypes.h>
-#include "errors.h"
+#include <stdarg.h>
 
 typedef double float64_t;
 
@@ -18,6 +18,28 @@ struct int_matrix
     size_t rows;
     size_t cols;
 };
+
+inline void free_int_matrix(struct int_matrix **matrix_to_delete)
+{
+    if (matrix_to_delete == NULL || *matrix_to_delete == NULL)
+        return;
+
+    struct int_matrix *matrix = *matrix_to_delete;
+
+    if (matrix->data != NULL)
+    {
+        for (size_t i = 0; i < matrix->rows; i++)
+        {
+            free(matrix->data[i]);
+        }
+        free(matrix->data);
+    }
+
+    free(matrix);
+    *matrix_to_delete = NULL;
+}
+
+
 
 inline struct int_matrix *create_int_matrix(const size_t rows, const size_t cols)
 {
@@ -107,6 +129,59 @@ inline int initialization_int_matrix(const struct int_matrix *matrix, const int6
     }
     return 0;
 }
+
+inline int initialization_float_matrix(const struct float_matrix *matrix, const float value)
+{
+    if (matrix == NULL || matrix->data == NULL || matrix->cols <= 0 || matrix->rows <= 0)
+    {
+        return -1;
+    }
+
+    for (size_t i = 0; i < matrix->rows; i++)
+    {
+        for (size_t j = 0; j < matrix->cols; j++)
+        {
+            matrix->data[i][j] = value;
+        }
+    }
+    return 0;
+}
+
+inline struct int_matrix *add_int_matrices(const size_t n, const size_t rows, const size_t cols, ...)
+{
+    va_list args;
+    va_start(args, cols);
+
+    struct int_matrix *matrix = create_int_matrix(rows, cols);
+    if (matrix == NULL)
+    {
+        va_end(args);
+        return NULL;
+    }
+
+    for (size_t k = 0; k < n; k++)
+    {
+        const struct int_matrix *arg = va_arg(args, struct int_matrix *);
+        if (arg == NULL || arg->data == NULL || arg->cols != cols || arg->rows != rows)
+        {
+            free_int_matrix(&matrix);
+
+            va_end(args);
+            return NULL;
+        }
+        for (size_t i = 0; i < rows; i++)
+        {
+            for (size_t j = 0; j < cols; j++)
+            {
+                matrix->data[i][j] += arg->data[i][j];
+            }
+        }
+    }
+
+    va_end(args);
+    return matrix;
+}
+
 
 void destroy_array_i_2d(int64_t ***ptr, size_t height);
 void destroy_array_f_2d(float64_t ***ptr, size_t height);
