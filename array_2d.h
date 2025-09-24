2e57aa5fbbc20f6dbc3d6155c7402fdb66e588d1
Andezion <245122@edu.p.lodz.pl>
2025-09-24 13:18:56 +0200

    added sub for matrix

--- array_2d.h
+++ array_2d.h
@@ -183,6 +183,41 @@ inline struct int_matrix *add_int_matrices(const size_t n, const size_t rows, co
 }
 
 
+inline struct int_matrix *sub_int_matrices(const size_t n, const size_t rows, const size_t cols, ...)
+{
+    va_list args;
+    va_start(args, cols);
+
+    struct int_matrix *matrix = create_int_matrix(rows, cols);
+    if (matrix == NULL)
+    {
+        va_end(args);
+        return NULL;
+    }
+
+    for (size_t k = 0; k < n; k++)
+    {
+        const struct int_matrix *arg = va_arg(args, struct int_matrix *);
+        if (arg == NULL || arg->data == NULL || arg->cols != cols || arg->rows != rows)
+        {
+            free_int_matrix(&matrix);
+
+            va_end(args);
+            return NULL;
+        }
+        for (size_t i = 0; i < rows; i++)
+        {
+            for (size_t j = 0; j < cols; j++)
+            {
+                matrix->data[i][j] -= arg->data[i][j];
+            }
+        }
+    }
+
+    va_end(args);
+    return matrix;
+}
+
 void destroy_array_i_2d(int64_t ***ptr, size_t height);
 void destroy_array_f_2d(float64_t ***ptr, size_t height);
 
