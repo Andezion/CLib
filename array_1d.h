ce90e56568c8630759a27f62695eb6ef57e535c3
Andezion <245122@edu.p.lodz.pl>
2025-09-30 12:25:26 +0200

    clean up some stuff

--- array_1d.h
+++ array_1d.h
@@ -38,7 +38,6 @@ inline struct int_array *create_int_array(const size_t size)
 
     return arr;
 }
-
 inline struct int_array *add_int_arrays(const size_t n, const size_t size, ...)
 {
     va_list args;
@@ -71,7 +70,6 @@ inline struct int_array *add_int_arrays(const size_t n, const size_t size, ...)
     va_end(args);
     return array;
 }
-
 inline struct int_array *sub_int_arrays(const size_t n, const size_t size, ...)
 {
     va_list args;
@@ -123,7 +121,6 @@ inline struct float_array *create_float_array(const size_t size)
 
     return arr;
 }
-
 inline struct float_array *add_float_arrays(const size_t n, const size_t size, ...)
 {
     va_list args;
@@ -156,7 +153,6 @@ inline struct float_array *add_float_arrays(const size_t n, const size_t size, .
     va_end(args);
     return array;
 }
-
 inline struct float_array *sub_float_arrays(const size_t n, const size_t size, ...)
 {
     va_list args;
@@ -203,7 +199,6 @@ inline int initialization_int_array(const struct int_array *arr, const int64_t v
     }
     return 0;
 }
-
 inline int initialization_float_array(const struct int_array *arr, const float64_t value)
 {
     if (arr == NULL || arr->data == NULL || arr->size <= 0)
@@ -218,15 +213,8 @@ inline int initialization_float_array(const struct int_array *arr, const float64
     return 0;
 }
 
-
 void destroy_array_i_1d(int64_t **ptr, size_t size);
 void destroy_array_f_1d(float64_t **ptr, size_t size);
-
-int create_array_i_1d(int64_t **ptr, size_t size);
-int create_array_f_1d_init(int64_t **ptr, size_t size, int64_t value);
-
-int create_array_f_1d(float64_t **ptr, size_t size);
-int create_array_f_1d_init(float64_t **ptr, size_t size, float64_t value);
 
 void display_array_i_1d(const int64_t *ptr, size_t size);
 void display_array_f_1d(const float64_t *ptr, size_t size);
@@ -234,22 +222,4 @@ void display_array_f_1d(const float64_t *ptr, size_t size);
 int64_t sum_array_i_1d(const int64_t *ptr, size_t size);
 float64_t sum_array_f_1d(const float64_t *ptr, size_t size);
 
-void add_to_array_i_1d_i(int64_t *ptr, size_t size, int64_t value);
-void add_to_array_i_1d_f(int64_t *ptr, size_t size, float64_t value);
-void add_to_array_f_1d_i(float64_t *ptr, size_t size, int64_t value);
-void add_to_array_f_1d_f(float64_t *ptr, size_t size, float64_t value);
 
-void mul_to_array_i_1d_i(int64_t *ptr, size_t size, int64_t value);
-void mul_to_array_i_1d_f(int64_t *ptr, size_t size, float64_t value);
-void mul_to_array_f_1d_i(float64_t *ptr, size_t size, int64_t value);
-void mul_to_array_f_1d_f(float64_t *ptr, size_t size, float64_t value);
-
-void sub_to_array_i_1d_i(int64_t *ptr, size_t size, int64_t value);
-void sub_to_array_i_1d_f(int64_t *ptr, size_t size, float64_t value);
-void sub_to_array_f_1d_i(float64_t *ptr, size_t size, int64_t value);
-void sub_to_array_f_1d_f(float64_t *ptr, size_t size, float64_t value);
-
-void div_to_array_i_1d_i(int64_t *ptr, size_t size, int64_t value);
-void div_to_array_i_1d_f(int64_t *ptr, size_t size, float64_t value);
-void div_to_array_f_1d_i(float64_t *ptr, size_t size, int64_t value);
-void div_to_array_f_1d_f(float64_t *ptr, size_t size, float64_t value);
