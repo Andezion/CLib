96625e85807a56b587fe4206a84ab90c1497c2c8
Andezion <245122@edu.p.lodz.pl>
2025-10-04 13:51:26 +0200

    added multiplication for arrays

--- array/array_math.c
+++ array/array_math.c
@@ -158,3 +158,80 @@ float64_t sum_float_array(const float64_t *ptr, const size_t size)
     }
     return sum;
 }
+
+struct int_array * hadamard_product_int_arrays(const size_t n, const size_t size, ...)
+{
+    va_list args;
+    va_start(args, size);
+
+    struct int_array *array = create_int_array(size);
+    if (array == NULL)
+    {
+        va_end(args);
+        return NULL;
+    }
+    const int error = initialization_int_array(array, 1);
+    if (error != 0)
+    {
+        free(array);
+
+        va_end(args);
+        return NULL;
+    }
+
+    for (size_t k = 0; k < n; k++)
+    {
+        const struct int_array *arr = va_arg(args, struct int_array *);
+        if (arr == NULL || arr->data == NULL || arr->size != size)
+        {
+            free(array);
+
+            va_end(args);
+            return NULL;
+        }
+
+        for (size_t i = 0; i < size; i++)
+        {
+            array->data[i] = array->data[i] * arr->data[i];
+        }
+    }
+
+    va_end(args);
+    return array;
+}
+
+int64_t dot_product_int_arrays(const size_t n, const size_t size, ...)
+{
+    va_list args;
+    va_start(args, size);
+
+    const struct int_array *first = va_arg(args, struct int_array *);
+    if (first == NULL || first->data == NULL || first->size != size)
+    {
+        va_end(args);
+        return 0;
+    }
+
+    int64_t sum = 0;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        int64_t prod = first->data[i];
+
+        for (size_t k = 1; k < n; k++)
+        {
+            const struct int_array *arr = va_arg(args, struct int_array *);
+            if (arr == NULL || arr->data == NULL || arr->size != size)
+            {
+                va_end(args);
+                return 0;
+            }
+            prod *= arr->data[i];
+        }
+
+        sum += prod;
+    }
+
+    va_end(args);
+    return sum;
+}
--- array/array_math.h
+++ array/array_math.h
@@ -9,3 +9,6 @@ inline struct float_array *sub_float_arrays(size_t n, size_t size, ...);
 
 int64_t sum_int_array(const int64_t *ptr, size_t size);
 float64_t sum_float_array(const float64_t *ptr, size_t size);
+
+inline int64_t dot_product_int_arrays(size_t n, size_t size, ...);
+inline struct int_array *hadamard_product_int_arrays(size_t n, size_t size, ...);
