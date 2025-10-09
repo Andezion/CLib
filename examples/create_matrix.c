da3316c15fe22ffeadd775f4706281a1e250b0e4
Andezion <245122@edu.p.lodz.pl>
2025-10-09 21:59:20 +0200

    added some examples

--- examples/create_matrix.c
+++ examples/create_matrix.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
+    printf("===== Matrix Int =====\n");
     struct int_matrix *matrix_int = create_int_matrix(5, 5);
     display_int_matrix(matrix_int);
     free_int_matrix(&matrix_int);
 
     printf("\n\n\n");
 
+    printf("===== Matrix Float =====\n");
     struct float_matrix *matrix_float = create_float_matrix(5, 5);
     display_float_matrix(matrix_float);
     free_float_matrix(&matrix_float);
