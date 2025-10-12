157cdc7db316ddb0b13c123e56ff37858b275c53
Andezion <245122@edu.p.lodz.pl>
2025-10-13 00:31:39 +0200

    finished sum examples

--- examples/sum_matrix.c
+++ examples/sum_matrix.c
@@ -24,7 +24,32 @@ int main()
     printf("===== Sum Matrix Int =====\n");
     sum = sum_int_matrix(matrix_int);
     printf("Sum is = %lld\n", sum);
-
     free_int_matrix(&matrix_int);
+
+    printf("\n\n\n");
+
+    printf("===== Matrix Float =====\n");
+    struct float_matrix *matrix_float = create_float_matrix(5, 5);
+    display_float_matrix(matrix_float);
+
+    printf("\n");
+
+    printf("===== Sum Matrix Float =====\n");
+    float64_t sum_float = sum_float_matrix(matrix_float);
+    printf("Sum is = %f\n", sum_float);
+
+    printf("\n\n\n");
+
+    printf("===== Initialized Matrix Float =====\n");
+    initialization_float_matrix(matrix_float, 6.2);
+    display_float_matrix(matrix_float);
+
+    printf("\n");
+
+    printf("===== Sum Matrix Float =====\n");
+    sum_float = sum_float_matrix(matrix_float);
+    printf("Sum is = %f\n", sum_float);
+    free_float_matrix(&matrix_float);
+
     return 0;
 }
