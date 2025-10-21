b0deab400505b1a6b4f644d2ae74147b1a4fde0f
Andezion <245122@edu.p.lodz.pl>
2025-10-21 11:17:17 +0200

    added new examples

--- examples/copy_matrix.c
+++ examples/copy_matrix.c
@@ -6,13 +6,21 @@ int main()
     struct int_matrix *matrix_int = create_int_matrix(5, 5);
     display_int_matrix(matrix_int);
 
-    printf("\n\n\n");
+    printf("\n");
 
     printf("===== Copy Matrix Int =====\n");
     struct int_matrix *copy_matrix_int = copy_int_matrix(matrix_int);
     display_int_matrix(copy_matrix_int);
+
+    printf("\n");
+
+    printf("===== Copy Matrix Int Error =====\n");
+    struct int_matrix *copy_matrix_int_error = copy_int_matrix(NULL);
+    display_int_matrix(copy_matrix_int_error);
+
     free_int_matrix(&copy_matrix_int);
     free_int_matrix(&matrix_int);
+    free_int_matrix(&copy_matrix_int_error);
 
     printf("\n\n\n");
 
@@ -20,12 +28,20 @@ int main()
     struct float_matrix *matrix_float = create_float_matrix(5, 5);
     display_float_matrix(matrix_float);
 
-    printf("\n\n\n");
+    printf("\n");
 
     printf("===== Copy Matrix Float =====\n");
     struct float_matrix *copy_matrix_float = copy_float_matrix(matrix_float);
     display_float_matrix(copy_matrix_float);
+
+    printf("\n");
+
+    printf("===== Copy Matrix Float Error =====\n");
+    struct float_matrix *copy_matrix_float_error = copy_float_matrix(NULL);
+    display_float_matrix(copy_matrix_float_error);
+
     free_float_matrix(&copy_matrix_float);
+    free_float_matrix(&copy_matrix_float_error);
     free_float_matrix(&matrix_float);
 
     return 0;
