e8cbd54ed73bd288ef02807ba31bbe501f346f00
Andezion <245122@edu.p.lodz.pl>
2025-10-27 08:53:06 +0100

    added new tests

--- examples/random_initialization_matrix.c
+++ examples/random_initialization_matrix.c
@@ -8,12 +8,26 @@ int main()
     struct int_matrix *matrix_int = create_int_matrix(5, 5);
     display_int_matrix(matrix_int);
 
-    printf("\n\n\n");
+    printf("\n");
 
     printf("===== Random Initialized Matrix Int =====\n");
     initialization_random_int_matrix(matrix_int, 0, 99);
     display_int_matrix(matrix_int);
     free_int_matrix(&matrix_int);
+
+    printf("\n");
+
+    printf("===== Error Matrix Int =====\n");
+    struct int_matrix *error_numbers_matrix_int = create_int_matrix(-1, -3);
+    display_int_matrix(error_numbers_matrix_int);
+    free_int_matrix(&error_numbers_matrix_int);
+
+    printf("\n");
+
+    printf("===== Initialized Error Matrix Int =====\n");
+    initialization_random_int_matrix(matrix_int, 0, 99);
+    display_int_matrix(error_numbers_matrix_int);
+    free_int_matrix(&error_numbers_matrix_int);
 
     printf("\n\n\n");
 
@@ -21,12 +35,26 @@ int main()
     struct float_matrix *matrix_float = create_float_matrix(5, 5);
     display_float_matrix(matrix_float);
 
-    printf("\n\n\n");
+    printf("\n");
 
     printf("===== Random Initialized Matrix Float =====\n");
     initialization_random_float_matrix(matrix_float, 0.0, 99.0);
     display_float_matrix(matrix_float);
     free_float_matrix(&matrix_float);
+
+    printf("\n");
+
+    printf("===== Error Matrix Float =====\n");
+    struct float_matrix *error_numbers_matrix_float = create_float_matrix(-1, -3);
+    display_float_matrix(error_numbers_matrix_float);
+    free_float_matrix(&error_numbers_matrix_float);
+
+    printf("\n");
+
+    printf("===== Initialized Error Matrix Float =====\n");
+    initialization_random_float_matrix(matrix_float, 0.0, 99.0);
+    display_float_matrix(error_numbers_matrix_float);
+    free_float_matrix(&error_numbers_matrix_float);
 
     return 0;
 }
