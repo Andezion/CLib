446f823148ba99a8b6e743d1f9be69521f944d74
Andezion <245122@edu.p.lodz.pl>
2025-11-20 01:28:00 +0100

    added new stuff to init arrays

--- examples/initialization_array.c
+++ examples/initialization_array.c
@@ -13,5 +13,18 @@ int main()
     display_int_array(array_int);
     free_int_array(&array_int);
 
+    printf("\n");
+
+    printf("===== Error Matrix Int =====\n");
+    struct int_array *array_int_error = create_int_array(-5);
+    display_int_array(array_int_error);
+
+    printf("\n");
+
+    printf("===== Error Initialized Matrix Int =====\n");
+    initialization_int_array(array_int_error, 5);
+    display_int_array(array_int_error);
+    free_int_array(&array_int_error);
+
     return 0;
 }
