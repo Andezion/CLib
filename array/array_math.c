b78d0032d1037653dc39a0bf7331e82f92ff9279
Andezion <245122@edu.p.lodz.pl>
2025-12-09 14:43:55 +0100

    fixed funcs

--- array/array_math.c
+++ array/array_math.c
@@ -187,14 +187,21 @@ struct int_array * sub_value_from_int_arrays(const size_t n, const size_t size, 
     va_list args;
     va_start(args, size);
 
-    struct int_array *array = create_int_array(size);
-    if (array == NULL)
+    struct int_array *array_input = va_arg(args, struct int_array *);
+    if (!array_input)
     {
         va_end(args);
         return NULL;
     }
 
-    for (size_t k = 0; k < n; k++)
+    struct int_array *array = copy_int_array(array_input);
+    if (!array)
+    {
+        va_end(args);
+        return NULL;
+    }
+
+    for (size_t k = 1; k < n; k++)
     {
         const struct int_array *arr = va_arg(args, struct int_array *);
         if (arr == NULL || arr->data == NULL || arr->size != size)
@@ -263,14 +270,21 @@ struct float_array * sub_value_from_float_arrays(const size_t n, const size_t si
     va_list args;
     va_start(args, size);
 
-    struct float_array *array = create_float_array(size);
-    if (array == NULL)
+    struct float_array *array_input = va_arg(args, struct float_array *);
+    if (!array_input)
     {
         va_end(args);
         return NULL;
     }
 
-    for (size_t k = 0; k < n; k++)
+    struct float_array *array = copy_float_array(array_input);
+    if (!array)
+    {
+        va_end(args);
+        return NULL;
+    }
+
+    for (size_t k = 1; k < n; k++)
     {
         const struct float_array *arr = va_arg(args, struct float_array *);
         if (arr == NULL || arr->data == NULL || arr->size != size)
