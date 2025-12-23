994086aeb51266c19acba23621226bb813dd6981
Andezion <245122@edu.p.lodz.pl>
2025-12-23 21:07:21 +0100

    added new example

--- examples_full_run/example_xor.c
+++ examples_full_run/example_xor.c
@@ -41,13 +41,15 @@ int main(void)
     const float64_t inputs[4][2] = {{0, 0},{0, 1},{1, 0},{1, 1}};
     const float64_t targets[4] = {0, 1, 1, 0};
 
-    const double lr = 0.8;
     for (int epoch = 0; epoch < 5000; epoch++)
     {
-        double loss = 0.0;
+        float64_t loss = 0.0;
         for (size_t i = 0; i < 4; i++)
         {
-            x->data[0] = inputs[i][0]; x->data[1] = inputs[i][1];
+            const float64_t lr = 0.8;
+
+            x->data[0] = inputs[i][0];
+            x->data[1] = inputs[i][1];
 
             dense_forward(l1, x, h);
             tanh_inplace(h);
@@ -60,8 +62,11 @@ int main(void)
 
             mse_grad(y, target, grad_out);
 
-            struct float_matrix *dW2 = NULL; struct float_array *db2 = NULL;
+            struct float_matrix *dW2 = NULL;
+            struct float_array *db2 = NULL;
+
             struct float_array *d_h = create_float_array(hid);
+
             dense_backward(l2, h, grad_out, &dW2, &db2, d_h);
 
             sgd_update_dense(l2, dW2, db2, lr);
@@ -69,30 +74,51 @@ int main(void)
             free_float_matrix(&dW2); free_float_array(&db2);
 
             struct float_array *d_h_pre = create_float_array(hid);
-            sigmoid_grad(h, d_h_pre); /* reuse as placeholder */
+            sigmoid_grad(h, d_h_pre);
 
-            struct float_matrix *dW1 = NULL; struct float_array *db1 = NULL;
+            struct float_matrix *dW1 = NULL;
+            struct float_array *db1 = NULL;
+
             struct float_array *d_x = create_float_array(in_dim);
+
             dense_backward(l1, x, d_h, &dW1, &db1, d_x);
 
             sgd_update_dense(l1, dW1, db1, lr);
 
-            free_float_matrix(&dW1); free_float_array(&db1);
-            free_float_array(&d_h); free_float_array(&d_h_pre); free_float_array(&d_x);
+            free_float_matrix(&dW1);
+            free_float_array(&db1);
+            free_float_array(&d_h);
+            free_float_array(&d_h_pre);
+            free_float_array(&d_x);
         }
-        if (epoch % 1000 == 0) printf("epoch %d loss=%.6f\n", epoch, loss);
+
+        if (epoch % 1000 == 0)
+        {
+            printf("epoch %d loss=%.6f\n", epoch, loss);
+        }
     }
 
     printf("Trained XOR predictions:\n");
     for (size_t i = 0; i < 4; i++)
     {
-        x->data[0] = inputs[i][0]; x->data[1] = inputs[i][1];
+        x->data[0] = inputs[i][0];
+        x->data[1] = inputs[i][1];
+
         dense_forward(l1, x, h); tanh_inplace(h);
         dense_forward(l2, h, y); sigmoid_inplace(y);
+
         printf("[%g,%g] -> %.3f (target %.0f)\n", inputs[i][0], inputs[i][1], y->data[0], targets[i]);
     }
 
-    dense_free(&l1); dense_free(&l2);
-    free_float_array(&x); free_float_array(&h); free_float_array(&y); free_float_array(&target); free_float_array(&grad_out);
+    dense_free(&l1);
+    dense_free(&l2);
+
+    free_float_array(&x);
+    free_float_array(&h);
+    free_float_array(&y);
+
+    free_float_array(&target);
+    free_float_array(&grad_out);
+
     return 0;
 }
