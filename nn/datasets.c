9173b9085ada1f3beed8c5199c6e22805a42132e
Andezion <245122@edu.p.lodz.pl>
2025-12-28 13:44:07 +0100

    added spiral shit

--- nn/datasets.c
+++ nn/datasets.c
@@ -8,34 +8,48 @@
 
 static double rnd_unit()
 {
-    return (double)rand() / (double)RAND_MAX;
+    return (float64_t)rand() / (float64_t)RAND_MAX;
 }
 
-int generate_spiral(size_t N, double *data, size_t *labels, size_t classes, double noise)
+int generate_spiral(const size_t N, double *data, size_t *labels, const size_t classes, const double noise)
 {
-    if (!data || !labels || classes < 2) return -1;
-    size_t per = N / classes;
+    if (!data || !labels || classes < 2)
+    {
+        return -1;
+    }
+
+    const size_t per = N / classes;
     size_t idx = 0;
 
     for (size_t c = 0; c < classes; c++)
     {
-        size_t count = per + (c < (N % classes) ? 1 : 0);
+        const size_t count = per + (c < N % classes ? 1 : 0);
         for (size_t i = 0; i < count; i++)
         {
-            double r = (double)i / (double)count;
-            double t = r * 4.0 * M_PI + (double)c * (2.0 * M_PI / (double)classes);
-            double x = r * cos(t);
-            double y = r * sin(t);
+            const float64_t r = (float64_t)i / (float64_t)count;
+            const float64_t t = r * 4.0 * M_PI + (float64_t)c * (2.0 * M_PI / (float64_t)classes);
+
+            float64_t x = r * cos(t);
+            float64_t y = r * sin(t);
+
             x += (rnd_unit() - 0.5) * noise;
             y += (rnd_unit() - 0.5) * noise;
 
             data[2 * idx + 0] = x;
             data[2 * idx + 1] = y;
+
             labels[idx] = c;
             idx++;
-            if (idx >= N) break;
+
+            if (idx >= N)
+            {
+                break;
+            }
         }
-        if (idx >= N) break;
+        if (idx >= N)
+        {
+            break;
+        }
     }
 
     return 0;
@@ -72,9 +86,10 @@ int generate_moons(const size_t N, float64_t *data, size_t *labels, const float6
 
     for (size_t i = 0; i < n2; i++)
     {
-        double t = M_PI * (double)i / (double)n2;
-        double x = 1.0 - cos(t);
-        double y = -sin(t) - 0.5;
+        const float64_t t = M_PI * (float64_t)i / (float64_t)n2;
+
+        float64_t x = 1.0 - cos(t);
+        float64_t y = -sin(t) - 0.5;
 
         x += (rnd_unit() - 0.5) * noise;
         y += (rnd_unit() - 0.5) * noise;
